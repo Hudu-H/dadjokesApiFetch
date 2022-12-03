6ca26e14f4715ab0a66799ac6d466f11b7d08cfb
Hudu Hamed <johndoe@example.com>
2022-12-03 13:09:21 +0100

    cleaned code base

--- lol.js
+++ lol.js
@@ -1,26 +1,18 @@
-const jokeE1 = document.getElementById('joke')
-const jokeBtn = document.getElementById('jokeBtn')
+const jokeE1 = document.getElementById("joke");
+const jokeBtn = document.getElementById("jokeBtn");
 
-jokeBtn.addEventListener('click',()=>{
-   generateJoke()
-   
-} )
-
-
+jokeBtn.addEventListener("click", () => {
+  generateJoke();
+});
 
 async function generateJoke() {
- 
-
- const res = await fetch('https://icanhazdadjoke.com', {
+  const res = await fetch("https://icanhazdadjoke.com", {
     headers: {
-        'Accept': 'application/json'
-    }
- })
- console.log(res)
- let data = await res.json()
- console.log(data);
- jokeE1.innerHTML = data.joke  
-
+      Accept: "application/json",
+    },
+  });
+  console.log(res);
+  let data = await res.json();
+  console.log(data);
+  jokeE1.innerHTML = data.joke;
 }
-
-
